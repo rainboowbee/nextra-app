8438bcc889d61a6ae1b4cb7e42749289d1ae98fd
rainboowbee <rainboowbee@gmail.com>
2025-03-04 07:18:51 +0300

    also fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  i18n: {
+    locales: ['ru'],
+    defaultLocale: 'ru'
+  }
+})
